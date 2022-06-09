c557b74db00484ae49996fe4a1aefec5dbb3274b
LeoGx <lgarrigoux@gmail.com>
2022-06-09 15:41:30 +0200

    Site with dynamic display on button clic

--- scripts.js
+++ scripts.js
@@ -2,15 +2,22 @@ const BASE_URL = "http://localhost:8000/api/v1/titles/";
 const BEST_URL = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score'
 const ACTION_URL = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=action";
 const FANTASY_URL = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=fantasy";
-const WAR_URL ="http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=war"
+const WAR_URL ="http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=war";
 
-let movielist = {};
+let moviesdata = {};
+let bestcount = 0;
+let page_best = 0;
+let page_action = 0;
+let page_fantasy = 0;
+let page_war = 0;
 
-async function getMovies(path) {// Load 2 pages of a category from API and save the movies contained.
+async function getMovies(path, page) {// Load 2 pages of a category from API and save the movies contained.
     var movies_list = [];
     var results = "";
-    for (var i = 0; i < 2; i++) {
-        var currentpath = `${path}&page=${i+1}`;
+    var startpage = page;
+    for (var i = 0; i < 14; i++) {
+        startpage++;
+        var currentpath = `${path}&page=${startpage}`;
         var response = await fetch(currentpath);
         var data = await response.json();
         results = data["results"];
@@ -33,7 +40,6 @@ async function getTopMovie() {//Gets the best movie id
 }
 
 async function getCover(movie) {// Get covers for display
-    console.log(movie);
     var title = movie["title"];
     var picture = movie["image_url"];
     var id = movie["id"];
@@ -44,14 +50,12 @@ async function getCover(movie) {// Get covers for display
 function displayMovies(list, element){
     var inner = "";
     var target = document.getElementById(element);
-            console.log(list[1]);
     for(var i = 0; i < 7; i++){
         var movie = list[i];
         var title = list[i]["title"];
         var picture = list[i]["image_url"];
         var id = list[i]["id"];
         movie = `<p><input type="image" src="${picture}" alt="${title}" id="${id}"/></p>`;
-        console.log(movie);
         inner = inner.concat(" ", movie)
     }
     target.innerHTML = inner;
@@ -69,22 +73,70 @@ async function displayBest(movieId) {//Show the best movie
     target_div.innerHTML = image;
 }
 
+async function nextPage(element, url){
+    if(element=="best_films"){
+        page_best++;
+        var page = page_best;
+    }else if(element=="action"){
+        page_action++;
+        var page = page_action;
+    }else if(element=="fantasy"){
+        page_fantasy++;
+        var page = page_fantasy;
+    }else if(element=="war"){
+        page_war++;
+        var page = page_war;
+    }
+    if(page>10){
+        alert("Vous avez atteint la fin de la liste");
+    }else{
+    Promise.all([getMovies(url, page)])
+    .then(([result]) => {
+        displayMovies(result, element);
+    });
+}
+}
 
-async function onLoading() {//Load all the datas required for page display.
-    Promise.all([getTopMovie(), getMovies(BEST_URL), getMovies(ACTION_URL),
-                getMovies(FANTASY_URL), getMovies(WAR_URL)])
+async function previousPage(element, url){
+    if(element=="best_films"){
+        page_best--;
+        var page = page_best;
+    }else if(element=="action"){
+        page_action--;
+        var page = page_action;
+    }else if(element=="fantasy"){
+        page_fantasy--;
+        var page = page_fantasy;
+    }else if(element=="war"){
+        page_war--;
+        var page = page_war;
+    }
+    if(page<0){
+        alert("Vous êtes au début de la liste");
+    }else{
+    Promise.all([getMovies(url, page)])
+    .then(([result]) => {
+        displayMovies(result, element);
+    });
+}
+}
+
+async function load() {
+    Promise.all([getTopMovie(), getMovies(BEST_URL, 0), getMovies(ACTION_URL, 0),
+                getMovies(FANTASY_URL, 0), getMovies(WAR_URL, 0)])
     .then(([result1, result2, result3, result4, result5]) => {
     displayBest(result1);
     displayMovies(result2, "best_films");
     displayMovies(result3, "action");
     displayMovies(result4, "fantasy");
     displayMovies(result5, "war");
-    movielist = {"best_films": result2,
+    var data = {"best_films": result2,
                  "action": result3,
                  "fantasy": result4,
                  "war": result5};
-});
-    }
+    return data;
+}
+);
+}
 
-
-onLoading();
+load();
